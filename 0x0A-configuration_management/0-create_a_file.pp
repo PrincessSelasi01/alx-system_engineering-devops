28fbf805ee883980bb83b7e9246f0eca40a6d126
Princess Selasi <selasiprincess99@gmail.com>
2022-09-16 19:08:11 -0200

    management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# create a file
-file { '/tmp/school':
-  ensure  => 'file',
-  content => 'I love Puppet',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
+#Create a file in /tmp
+
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
